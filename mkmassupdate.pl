0e34e3736ee758bf46c83c10e11e6098c71ddc91
Gabriel Rolland <gabriel@resnovae.it>
2023-12-19 15:47:30 +0100

    Version 2.1

--- mkmassupdate.pl
+++ mkmassupdate.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 ###################################################
-#         MikroTik Mass Updater v2.0
+#         MikroTik Mass Updater v2.1
 #         Original Written by: Phillip Hutchison
 #         Revamped version by: Kevin Byrd 
 #         Ported to OpenSSH by: Gabriel Rolland
@@ -9,8 +9,8 @@
 
 use strict;
 use warnings;
-use Net::OpenSSH;
-use Parallel::ForkManager;
+use Net::OpenSSH::Parallel;
+
 use constant USERNAME => 'admin';
 use constant PASSWORD => 'password';
 use constant IP_LIST_FILE => 'list.txt';
@@ -19,36 +19,25 @@ use constant LOG_FILE => 'backuplog.txt';
 open(FH, IP_LIST_FILE) or die "Can't open the list file $!\n";
 open(LOG, ">>", LOG_FILE) or print "Couldn't open log file: $!";
 
-my $pm = Parallel::ForkManager->new(5);  # Set the number of parallel processes
+my $pssh = Net::OpenSSH::Parallel->new();
 
 while (my $line = <FH>) {
-    $pm->start and next;
+   chomp($line);
+   my ($IP, $port) = split(':', $line);
 
-    chomp($line);
-    my ($IP, $port) = split(':', $line);
+   $pssh->add_host($IP, user => USERNAME, password => PASSWORD, port => 22, timeout => 3, kill_ssh_on_timeout => 1, master_opts => [-o => "StrictHostKeyChecking=no"]);
 
-    my $ssh = Net::OpenSSH->new("$IP", user => USERNAME, password => PASSWORD, port  => 22, timeout => 3, kill_ssh_on_timeout => 1, master_opts => [-o => "StrictHostKeyChecking=no"]);
-
-    print "Connecting to ".$IP."\n";
-    print LOG "Connecting to $IP\n";
-
-    if ($ssh->error) {
-        print "Couldn't establish SSH connection: ". $ssh->error;
-        print LOG "Couldn't establish SSH connection: ". $ssh->error."\n";
-        print LOG $ssh->error."\n";
-    } else {         
-        $ssh->system('/system identity print');
-        $ssh->system('/system package update check-for-updates');
-        $ssh->system('/system package update install');
-        #$ssh->system('/interface sstp-client set max-mtu=1400 0');
-        #$ssh->system('/interface pptp-client set max-mtu=1400 0');
-        $ssh->disconnect;
-        print "\n";
-    }
-
-    $pm->finish;
+   $pssh->push($IP, command => '/system identity print');
+   $pssh->push($IP, command => '/system package update check-for-updates');
+   $pssh->push($IP, command => '/system package update install');
+   $pssh->push($IP, command => 'quit');
 }
 
-$pm->wait_all_children;
+if (!$pssh->run) {
+   print "Error \n";
+}
+
+$pssh->run;
+
 close(FH);
 close(LOG);
